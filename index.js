2d89612573ecbf49873550e7c33b0a0389c4fe6e
unknown <rakibulhasan.seu.cse@gmail.com>
2023-05-21 06:04:27 +0600

    delete api and query search result api  done

--- index.js
+++ index.js
@@ -86,7 +86,13 @@ async function run() {
       res.send(result);
     });
 
-    //
+    // delete data
+    app.delete("/toys/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await alltoys.deleteOne(query);
+        res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
