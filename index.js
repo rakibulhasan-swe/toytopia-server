950888442a777fb1d91713ee1a58f31822dcc2b3
unknown <rakibulhasan.seu.cse@gmail.com>
2023-05-21 17:34:11 +0600

    vercel deployment done

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     // database and collection/table
     const alltoys = client.db("toytopia").collection("all-toys");
